c4ced378816650f5e4b6483175d47ac9554cc33c
ktanaka <spiralray@rakusei.net>
2015-02-11 15:16:01 +0900

    Add shuttle_kalman node.

---
+++ shuttle_kalman/scripts/shuttle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2015/02/11
+
+@author: spiralray
+'''
+
+
+import numpy as np
+
+class Shuttle:
+    resist_coeff = 0.001075
+    gravity = 9.812
+    mass = 0.005
+    
+    def __init__(self, mu):
+        self.mu = mu
+        
+        self.Sigma = np.mat([[0,0],[0,0]])
+        
+        self.B = np.mat([
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,-self.gravity] ])
+        
+        self.Q = np.eye(9)
+        self.R = np.eye(9)
+
+    def getA(self, mu, period):   #Observation matrix
+        
+        airR = -self.resist_coeff * (mu[3]**2 + mu[4]**2 + mu[5]**2 ) / self.mass
+        
+        return np.mat([
+            [1,0,0,period,0,0,0,0,0],
+            [0,1,0,0,period,0,0,0,0],
+            [0,0,1,0,0,period,0,0,0],
+            [0,0,0,1,0,0,period,0,0],
+            [0,0,0,0,1,0,0,period,0],
+            [0,0,0,0,0,1,0,0,period],
+            [0,0,0,airR,0,0,0,0,0],
+            [0,0,0,0,airR,0,0,0,0],
+            [0,0,0,0,0,airR,0,0,0]
+        ])
+    
+    def Jh(self, mu):   #Observation matrix
+        return np.mat([
+            [1,0,0],
+            [0,1,0],
+            [0,0,1],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ])
+        
+    def predict(self,period):
+        
+        self.A = self.getA(self.mu,period)
+        self.u = np.mat([[0],[0],[0],[0],[0],[0],[0],[0],[period]])
+        # prediction
+        self.mu = self.A * self.mu + self.B * self.u
+        self.Sigma_ = self.Q + self.A * self.Sigma * self.A.T
+        
+    def update(self, Y):    #Y Observation value
+        self.C = self.Jh(self.mu)
+        self.yi = Y - self.C * self.mu
+        self.S = self.C * self.Sigma_ * self.C.T + self.R
+        self.K = self.Sigma_ * self.C.T * self.S.I
+        self.mu = self.mu + self.K * self.yi
+        self.Sigma = self.Sigma_ - self.K * self.C * self.Sigma
+        
+    def getState(self):
+        return self.mu
+
