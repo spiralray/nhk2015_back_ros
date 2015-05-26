dbb84db18921b8872eaf48c6b3fd49de49dba51e
Kentaro Tanaka <spiralray@rakusei.net>
2015-05-26 18:45:22 +0900

    Fix kondo node

--- kondo/script/kondo.py
+++ kondo/script/kondo.py
@@ -25,16 +25,19 @@ class kondo(serial.Serial):
     def setAngle(self, id, value):
         self.cmd = chr(0x80 | id) + chr(int(value)/128) + chr(int(value)%128)
         self.write( self.cmd )
-        self.read(3)
-        self.rec = self.read(3)
-        return ord(self.rec[1])*0x80 + ord(self.rec[2])
+        self.rec = self.read(6)
+        return ord(self.rec[4])*0x80 + ord(self.rec[5])
 
 def callback(msg):
     pubmsg = servo()
     pubmsg.stamp = rospy.get_rostime()
     pubmsg.id = msg.id
-    pubmsg.angle = (k.setAngle(msg.id, 7500 + msg.angle/(0.000589049) )-7500) * (0.000589049)
-    pub.publish( pubmsg )
+    try:
+        pubmsg.angle = (k.setAngle(msg.id, 7500 + msg.angle/(0.000589049) )-7500) * (0.000589049)
+        pub.publish( pubmsg )
+        
+    except:
+        rospy.logerr("Communication error")
     
 if __name__ == '__main__':
     argv = rospy.myargv(sys.argv)
@@ -47,7 +50,7 @@ if __name__ == '__main__':
         rospy.logerr("Set correct port to %s", rospy.resolve_name('~port'))
         exit()
     
-    k = kondo(port, 115200, timeout=0.1, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE)
+    k = kondo(port, 115200, timeout=0.15, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE)
     
     pub = rospy.Publisher('/servo/rx', servo, queue_size=100)
     rospy.Subscriber("/servo/tx", servo, callback)
